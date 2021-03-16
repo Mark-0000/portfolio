1a826b94c1dbedb0b4a03020b0e5e4069897d969
Mark Christian Albinto <markchristianalbinto@yahoo.com>
2021-03-16 20:33:57 +0000

    Created class based views

--- ui/urls.py
+++ ui/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.Index.as_view(), name='index'),
+    # path('', views.home, name='index'),
     path('gallery/<int:project_id>/', views.gallery, name='gallery')
+
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from .forms import ContactForm
 from django.urls import reverse
 from .models import Project, Skill, Service, Gallery
+from django.contrib import messages
 
 
 # Create your views here.
-def home(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-        else:
-            return redirect(reverse('index') + '#contact')
-    else:
-        pass
-
-    context = {
-        'projects': Project.objects.all(),
-        'skills': Skill.objects.all(),
-        'services': Service.objects.all()
-    }
-    return render(request, 'ui/index.html', context)
+# def home(request):
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#         else:
+#             return redirect(reverse('index') + '#contact')
+#     else:
+#         pass
+#
+#     context = {
+#         'projects': Project.objects.all(),
+#         'skills': Skill.objects.all(),
+#         'services': Service.objects.all()
+#     }
+#     return render(request, 'ui/index.html', context)
 
 
 def gallery(request, project_id):
@@ -31,3 +33,22 @@ def gallery(request, project_id):
         'gallery': my_gallery
     }
     return render(request, 'ui/gallery.html', context)
+
+
+class Index(View):
+
+    def get(self, request):
+        context = {
+            'projects': Project.objects.all(),
+            'skills': Skill.objects.all(),
+            'services': Service.objects.all()
+        }
+        return render(request, 'ui/index.html', context)
+
+    def post(self, request):
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        context = {'form': form}
+        messages.success(request, f'Message Sent!')
+        return redirect(reverse('index') + '#contact', context)
